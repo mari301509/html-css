42da8e2a5a18598d8ea1ed24d1dc998987457fc4
mari301509 <lmr301509@gmail.com>
2020-03-24 16:05:55 +0900

    main section layout 구현 로그인, 유효성 검사, 웹 용어, 검색, 게시판 구현

--- js/main.js
+++ js/main.js
@@ -2,13 +2,26 @@
 var menuItems = $('.menu-item');
 var menuLinks = $('.menu-link');
 var subLinks = $('.submenu a');
+var boardItem2 = $('.board-item')
+var boardLink2 = $('.board-link')
+var boardLinks2 = $('.board-item > ul > li > a');
+var boardItem = $('.board-item')
+var boardLink = $('.')
+var boardLinks = $('.board-act > ul > li > a > span');
 
 //attr
 subLinks.attr('class','icon-dot-circled');
+boardLinks2.attr('class','icon-dot-circled');
+boardLinks.attr('class','icon-dot-circled');
 
 //funtion
 menuLinks.click(function(event){
     event.preventDefault();
     menuItems.removeClass('menu-item-act');
     $(this).parent().addClass('menu-item-act');
+})
+boardLink2.click(function(event){
+    event.preventDefault();
+    boardItem2.removeClass('board-act');
+    $(this).parent().addClass('board-act');
 })
